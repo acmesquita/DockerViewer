5d2a33882c676340a5258acedfc50b87ea4f542d
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2018-08-09 09:43:12 -0300

    Adicionando role e name

---
+++ db/migrate/20180809123013_add_user_name_and_role_to_user.rb
@@ -0,0 +1,6 @@
+class AddUserNameAndRoleToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :role, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_142310) do
+ActiveRecord::Schema.define(version: 2018_08_09_123013) do
 
   create_table "containers", force: :cascade do |t|
     t.string "container_id"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2018_08_06_142310) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
